db22c82f5fd950c59e18dd27a9b0031ed7eaab71
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-13 21:59:51 +0900

    Learn c++ class

---
+++ basic2/class.cpp
@@ -0,0 +1,11 @@
+#include "class.hpp"
+
+void CSample::set(int num) 
+{
+  m_num = num;
+}
+
+int CSample::get()
+{
+  return m_num;
+}
---
+++ basic2/class.hpp
@@ -0,0 +1,13 @@
+#ifndef _class_h_
+#define _class_h_
+
+class CSample
+{
+  public:
+    void set(int num);
+    int get();
+  private:
+    int m_num;
+};
+
+#endif
